ba2ab6e4081e33cbabc887b1df3d7acb5bc2ce7c
Amandeep Singh <amandeep.singh40@ad.infosys.com>
2019-04-20 00:52:43 +0530

    Updates

---
+++ precache-manifest.97fb3a5dc4c8e69936fae5b980c21dbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "390a7f3d32e74c648bb8",
+    "url": "/ReactPagination/static/js/runtime~main.a4087b03.js"
+  },
+  {
+    "revision": "0512438c536b340a5b82",
+    "url": "/ReactPagination/static/js/main.d1e86936.chunk.js"
+  },
+  {
+    "revision": "cbca904fe688fbd5bcfd",
+    "url": "/ReactPagination/static/js/2.8c839308.chunk.js"
+  },
+  {
+    "revision": "0512438c536b340a5b82",
+    "url": "/ReactPagination/static/css/main.c852c482.chunk.css"
+  },
+  {
+    "revision": "cbca904fe688fbd5bcfd",
+    "url": "/ReactPagination/static/css/2.e3053e51.chunk.css"
+  },
+  {
+    "revision": "0dea5063ed7ca93aa1ac3cc0b42d167c",
+    "url": "/ReactPagination/index.html"
+  }
+];
